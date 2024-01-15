3b8dda20886fd16380982f22c5cdfaeb9d8ffb20
Rainer Simon <rainer@rainersimon.io>
2024-01-15 14:41:36 +0100

    More hacks...

--- packages/pdf-annotator/src/api/scrollIntoView.ts
+++ packages/pdf-annotator/src/api/scrollIntoView.ts
@@ -10,29 +10,32 @@ export const scrollIntoView = <E extends unknown>(
 
   // Always scroll to page first
   const page = document.querySelector(`.page[data-page-number="${p}"]`);
-  page?.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
-  const success = anno.scrollIntoView(annotation);
+  setTimeout(() => {
+    page?.scrollIntoView({ behavior: 'smooth', block: 'nearest' }) 
+    const success = anno.scrollIntoView(annotation);
 
-  // If the jump wasn't successful, just jump to the page
-  // instead. (The annotation wasn't rendered, but PDF.js
-  // will have created the page container DIV already!)
-  if (!success) {
-    // We'll listen to textlayerrendered events until our target page (+1) comes up
-    const onTextLayerRendered =  ({ pageNumber }: { pageNumber: number }) => {
-      if (pageNumber === p) {
-        // Follow up scroll
-        setTimeout(() => anno.scrollIntoView(annotation), 500);
+    // If the jump wasn't successful, just jump to the page
+    // instead. (The annotation wasn't rendered, but PDF.js
+    // will have created the page container DIV already!)
+    if (!success) {
+      // We'll listen to textlayerrendered events until our target page (+1) comes up
+      const onTextLayerRendered =  ({ pageNumber }: { pageNumber: number }) => {
+        if (pageNumber === p) {
+          // Follow up scroll
+          setTimeout(() => anno.scrollIntoView(annotation), 500);
 
-        // Unregister this listener
-        eventBus.off('textlayerrendered', onTextLayerRendered);
+          // Unregister this listener
+          eventBus.off('textlayerrendered', onTextLayerRendered);
+        }
       }
+
+      eventBus.on('textlayerrendered', onTextLayerRendered);  
+
+      // Just to ensure we don't have dangling listeners
+      setTimeout(() => eventBus.off('textlayerrendered', onTextLayerRendered), 2000);
     }
 
-    eventBus.on('textlayerrendered', onTextLayerRendered);  
-
-    // Just to ensure we don't have dangling listeners
-    setTimeout(() => eventBus.off('textlayerrendered', onTextLayerRendered), 2000);
-  }
+  }, 1);
 
 }

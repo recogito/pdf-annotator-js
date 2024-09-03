9079b67813dced4d4e05d91a07b9e66840daf8bb
Rainer Simon <rainer@rainersimon.io>
2024-09-03 11:35:03 +0200

    Completing the API

--- packages/pdf-annotator/src/PDFAnnotator.ts
+++ packages/pdf-annotator/src/PDFAnnotator.ts
@@ -10,6 +10,7 @@ import {
   createBaseAnnotator, 
   createLifecycleObserver, 
   createUndoStack, 
+  Filter, 
   User 
 } from '@annotorious/core';
 import { addResizeObserver } from './responsive';
@@ -17,11 +18,16 @@ import type { PDFScale } from './PDFScale';
 import { PDFAnnotation } from './PDFAnnotation';
 import { createPDFViewer } from './createPDFViewer';
 import { createPDFAnnotatorState } from './state/PDFAnnotatorState';
+import { 
+  setScale as _setScale,
+  zoomIn as _zoomIn,
+  zoomOut as _zoomOut
+ } from './api';
 
 import './PDFAnnotator.css';
 import '@recogito/text-annotator/dist/text-annotator.css';
 
-export interface PDFAnnotator extends TextAnnotator<PDFAnnotation> {
+export interface PDFAnnotator extends TextAnnotator<PDFAnnotation, PDFAnnotation> {
 
   element: HTMLElement;
 
@@ -29,13 +35,15 @@ export interface PDFAnnotator extends TextAnnotator<PDFAnnotation> {
 
   currentScaleValue: string | undefined;
 
+  scrollIntoView(annotation: PDFAnnotation): boolean;
+
   setScale(scale: PDFScale | number): number;
 
   setStyle(style: HighlightStyleExpression | undefined): void;
 
   zoomIn(percentage?: number): number;
 
-  zoomOut(percentag?: number): number;
+  zoomOut(percentage?: number): number;
 
 }
 
@@ -43,7 +51,7 @@ export const createPDFAnnotator = (
   container: HTMLDivElement, 
   pdfURL: string,
   options: TextAnnotatorOptions<PDFAnnotation, PDFAnnotation> = {}
-) => createPDFViewer(container, pdfURL).then(({ viewer, viewerElement }) => {
+): Promise<PDFAnnotator> => createPDFViewer(container, pdfURL).then(({ viewer, viewerElement }) => {
   const opts = fillDefaults<PDFAnnotation, PDFAnnotation>(options, {
     annotatingEnabled: true
   });
@@ -102,6 +110,18 @@ export const createPDFAnnotator = (
     removeResizeObserver();
   }
 
+  const scrollIntoView = (annotation: PDFAnnotation) => {
+    // TODO
+    return true;
+  }
+
+  const setFilter = (filter?: Filter) => {
+    highlightRenderer.setFilter(filter);
+    selectionHandler.setFilter(filter);
+  }
+
+  const setScale = _setScale(viewer);
+
   const setStyle = (style: HighlightStyleExpression | undefined) =>
     highlightRenderer.setStyle(style);
 
@@ -110,6 +130,13 @@ export const createPDFAnnotator = (
     selectionHandler.setUser(user);
   }
 
+  const setVisible = (visible: boolean) =>
+    highlightRenderer.setVisible(visible);
+
+  const zoomIn = _zoomIn(viewer);
+  
+  const zoomOut = _zoomOut(viewer);
+
   return {
     ...base,
     element: viewerElement,
@@ -117,10 +144,16 @@ export const createPDFAnnotator = (
     get currentScaleValue() { return viewer.currentScaleValue },
     destroy,
     getUser,
-    setStyle,
-    setUser,
     on: lifecycle.on,
     off: lifecycle.off,
+    setFilter,
+    setScale,
+    setStyle,
+    setUser,
+    setVisible,
+    scrollIntoView,
+    zoomIn,
+    zoomOut,
     state
   }
 
--- packages/pdf-annotator/src/api/index.ts
+++ packages/pdf-annotator/src/api/index.ts
@@ -1,16 +1,1 @@
-import type { TextAnnotator } from '@recogito/text-annotator';
-import type { EventBus, PDFViewer } from 'pdfjs-dist/web/pdf_viewer.mjs';
-import { setScale, zoomIn, zoomOut } from './scale';
-import { scrollIntoView } from './scrollIntoView';
-import { PDFAnnotator } from '../PDFAnnotator';
-
-export const createAPI = (anno: PDFAnnotator, viewer: PDFViewer) => {
-
-  return {
-    scrollIntoView: scrollIntoView(anno, viewer.eventBus),
-    setScale: setScale(viewer),
-    zoomIn: zoomIn(viewer),
-    zoomOut: zoomOut(viewer)
-  }
-
-}
+export * from './scale';
